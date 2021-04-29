4443366245d9441dcfa030e25bf3cb805f1c2cb5
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2021-04-29 14:32:13 +0100

    fix scripts

--- compile-tests-xml.sh
+++ compile-tests-xml.sh
@@ -7,10 +7,20 @@ NC='\033[0m'
 total=0
 passed=0
 
+make
+
 cd tests
 for f in *.fir; do
     total=$(($total+1))
-    ../fir $f --target xml && (echo "$f ${GREEN}passed${NC}") && passed=$(($passed+1)) || (echo "$f ${RED}compilation error${NC}"; continue)
+
+    if ../fir $f --target xml; then
+        echo "$f ${GREEN}passed${NC}"
+        passed=$((passed+1))
+    else
+        echo "$f ${RED}compilation error${NC}"
+    fi
+
+    rm "${f%.*}.xml"
 done
 
 echo "Tests passed: $passed/$total"
--- run-tests.sh
+++ run-tests.sh
@@ -6,18 +6,23 @@ passed=0
 cd tests
 for f in *.fir; do
     total=$(($total+1))
-    
-    ../fir --target asm $f || (echo "$f compilation error" ; continue)
-    yasm -felf32 "${f%.*}.asm" || (echo "$f compilation error" ; continue)
-    ld -melf_i386 -o test_fir  "${f%.*}.o" -lrts || (echo "$f compilation error" ; continue)
+
+    if ../fir --target asm $f && yasm -felf32 "${f%.*}.asm" && ld -melf_i386 -o test_fir "${f%.*}.o" -lrts ; then
+        if ! ./test_fir > "${f%.*}.out" ; then
+            echo "$f execution error"
+        elif [ -z "$(diff "${f%.*}.out" "expected/${f%.*}.out")" ]; then
+            echo "$f passed"
+            passed=$(($passed+1))
+        else
+            echo "$f failed"
+        fi
+
+        rm "${f%.*}.out"
+        rm test_fir
+    fi
+
     rm "${f%.*}.o"
-
-    ./test_fir > "${f%.*}.out" || (echo "$f execution error" ; continue)
-
-    [ -z "$(diff "${f%.*}.out" "expected/${f%.*}.out")" ] && (echo "$f passed"; passed=$(($passed+1))) || echo "$f failed"
-
-    rm "${f%.*}.out"
-    rm test_fir
+    rm "${f%.*}.asm"
 done
 
 echo "Tests passed: $passed/$total"
