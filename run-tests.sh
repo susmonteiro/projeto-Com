2e23ca7d6afeddc1be4ec970d2696daa92b480d6
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2021-05-23 18:25:46 +0100

    add colors and compilation error info

--- run-tests.sh
+++ run-tests.sh
@@ -2,30 +2,39 @@
 
 total=0
 passed=0
+compiled=0
 
 make || exit 1
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 
 cd tests
 for f in *.fir; do
     total=$(($total+1))
 
     if ../fir --target asm $f && yasm -felf32 "${f%.*}.asm" && ld -melf_i386 -o test_fir "${f%.*}.o" -lrts ; then
+    	compiled=$(($compiled+1))
 	./test_fir > "${f%.*}.out"
         #if ! ./test_fir > "${f%.*}.out" ; then
         #    echo "$f execution error"
         if [ -z "$(diff "${f%.*}.out" "expected/${f%.*}.out")" ]; then
-            echo "$f passed"
+	    echo "$f ${GREEN}passed${NC}"
             passed=$(($passed+1))
         else
-            echo "$f failed"
+	    echo "$f ${RED}failed${NC}"
         fi
 
         rm "${f%.*}.out" &> /dev/null
         rm test_fir &> /dev/null
+    else
+        echo "$f ${RED}compilation error${NC}"
     fi
 
     rm "${f%.*}.o" &> /dev/null
     rm "${f%.*}.asm" &> /dev/null
 done
 
+echo "Tests compiled: $compiled/$total"
 echo "Tests passed: $passed/$total"
