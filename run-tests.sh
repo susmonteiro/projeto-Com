8863dcf12b4b44dcc329cdfb8d7e0229b67708f4
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2021-05-23 18:12:36 +0100

    minor fixes

--- run-tests.sh
+++ run-tests.sh
@@ -10,21 +10,22 @@ for f in *.fir; do
     total=$(($total+1))
 
     if ../fir --target asm $f && yasm -felf32 "${f%.*}.asm" && ld -melf_i386 -o test_fir "${f%.*}.o" -lrts ; then
-        if ! ./test_fir > "${f%.*}.out" ; then
-            echo "$f execution error"
-        elif [ -z "$(diff "${f%.*}.out" "expected/${f%.*}.out")" ]; then
+	./test_fir > "${f%.*}.out"
+        #if ! ./test_fir > "${f%.*}.out" ; then
+        #    echo "$f execution error"
+        if [ -z "$(diff "${f%.*}.out" "expected/${f%.*}.out")" ]; then
             echo "$f passed"
             passed=$(($passed+1))
         else
             echo "$f failed"
         fi
 
-        rm "${f%.*}.out"
-        rm test_fir
+        rm "${f%.*}.out" &> /dev/null
+        rm test_fir &> /dev/null
     fi
 
-    rm "${f%.*}.o"
-    rm "${f%.*}.asm"
+    rm "${f%.*}.o" &> /dev/null
+    rm "${f%.*}.asm" &> /dev/null
 done
 
 echo "Tests passed: $passed/$total"
