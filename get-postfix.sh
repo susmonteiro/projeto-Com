00e94de4195474dec63be042999a186430a45197
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2021-05-24 21:47:40 +0100

    add script to compile to postfix

---
+++ get-postfix.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+make &> /dev/null || exit 1
+
+cp $1 .
+f=$(basename $1)
+
+./fir -g --target asm $f
+cat ${f%.*}.asm | grep -P "^;" | cut -c3-
+
+rm "$f" &> /dev/null
+rm "${f%.*}.asm" &> /dev/null
