e311d5847a96cf802769a9336735cf7ba4d2eb8b
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2021-05-26 19:27:43 +0100

    add status code validation

--- run-one-test.sh
+++ run-one-test.sh
@@ -10,16 +10,18 @@ cp $1 .
 f=$(basename $1)
 
 if ./fir --target asm $f && yasm -felf32 "${f%.*}.asm" && ld -melf_i386 -o test_fir "${f%.*}.o" -lrts ; then
-    ./test_fir > "${f%.*}.out"
-
-    DIFF=$(diff -wBb <(tr -d '\n' < "${f%.*}.out") <(tr -d '\n' < "tests/expected/${f%.*}.out"))
-
-    if [ -z "$DIFF" ]; then
-        echo "${GREEN}passed${NC}"
-        passed=$(($passed+1))
+    if ! ./test_fir > "${f%.*}.out" ; then
+        echo "$f exited with code 1"
     else
-        echo "${RED}failed${NC}"
-        echo "$DIFF"
+        DIFF=$(diff -wBb <(tr -d '\n' < "${f%.*}.out") <(tr -d '\n' < "tests/expected/${f%.*}.out"))
+
+        if [ -z "$DIFF" ]; then
+            echo "${GREEN}passed${NC}"
+            passed=$(($passed+1))
+        else
+            echo "${RED}failed${NC}"
+            echo "$DIFF"
+        fi
     fi
 
     rm test_fir &> /dev/null
